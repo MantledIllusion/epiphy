aab729d66024f87a8b556f6ab1367e493317559f
Tobias Weber <weber.tobias@live.de>
2019-09-30 17:33:23 +0200

    JavaDoc (Context)

--- src/main/java/com/mantledillusion/data/epiphy/context/Context.java
+++ src/main/java/com/mantledillusion/data/epiphy/context/Context.java
@@ -3,17 +3,20 @@ package com.mantledillusion.data.epiphy.context;
 import java.util.IdentityHashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import com.mantledillusion.data.epiphy.Property;
 import com.mantledillusion.data.epiphy.context.reference.PropertyReference;
 
 /**
- * A pool {@link PropertyReference}s that describe a {@link Context} in which {@link Property}s should be evaluated.
+ * An immutable pool of {@link PropertyReference}s that describe the {@link Context} in which {@link Property}s should
+ * be evaluated.
  * <p>
  * {@link Context} are basically a {@link Map} of {@link Property}-&gt;{@link PropertyReference} pairs, represented by
  * implementations of {@link PropertyReference}.
  * <p>
- * Note that two {@link Property}s are only considered equal when they are the exact same {@link Object} instance.
+ * Note that two {@link Property}s are only considered equal when they are the exact same {@link Object} instance; as a
+ * result, the {@link Property}s in {@link Context}s are used by their {@link Object} identity.
  */
 public class Context {
 
@@ -31,7 +34,8 @@ public class Context {
 	/**
 	 * Returns whether this {@link Context} contains a reference for the given {@link Property}.
 	 *
-	 * @param property The {@link Property}; might be null.
+	 * @param property
+	 * 			The {@link Property}; might be null.
 	 * @return True if there is a reference, false otherwise
 	 */
 	public boolean containsReference(Property<?, ?> property) {
@@ -39,11 +43,15 @@ public class Context {
 	}
 
 	/**
-	 * Returns whether this {@link Context} contains a reference for the given {@link Property}.
+	 * Returns whether this {@link Context} contains a reference for the given {@link Property} that is an instance of
+	 * the given {@link PropertyReference} implementation {@link Class} type.
 	 *
-	 * @param <R> The {@link PropertyReference} implementation type.
-	 * @param property The {@link Property}; might be null.
-	 * @param referenceType The {@link PropertyReference} implementation {@link Class}; might <b>not</b> be null.
+	 * @param <R>
+	 * 			The {@link PropertyReference} implementation type.
+	 * @param property
+	 * 			The {@link Property}; might be null.
+	 * @param referenceType
+	 * 			The {@link PropertyReference} implementation {@link Class}; might <b>not</b> be null.
 	 * @return True if there is a reference, false otherwise
 	 */
 	public <R extends PropertyReference<?, ?>> boolean containsReference(Property<?, ?> property, Class<R> referenceType) {
@@ -56,8 +64,10 @@ public class Context {
 	/**
 	 * Returns The {@link PropertyReference} contained by this {@link Context} for the given {@link Property}.
 	 *
-	 * @param <R> The {@link PropertyReference} implementation type.
-	 * @param property The {@link Property}; might be null.
+	 * @param <R>
+	 * 			The {@link PropertyReference} implementation type.
+	 * @param property
+	 * 			The {@link Property}; might be null.
 	 * @return The reference, might be null
 	 */
 	public <R extends PropertyReference<?, ?>> R getReference(Property<?, ?> property) {
@@ -67,9 +77,12 @@ public class Context {
 	/**
 	 * Returns The {@link PropertyReference} contained by this {@link Context} for the given {@link Property}.
 	 *
-	 * @param <R> The {@link PropertyReference} implementation type.
-	 * @param property The {@link Property}; might be null.
-	 * @param referenceType The type of the {@link PropertyReference} to get; might <b>not</b> be null.
+	 * @param <R>
+	 * 			The {@link PropertyReference} implementation type.
+	 * @param property
+	 * 			The {@link Property}; might be null.
+	 * @param referenceType
+	 * 			The type of the {@link PropertyReference} to get; might <b>not</b> be null.
 	 * @return The reference, might be null
 	 */
 	public <R extends PropertyReference<?, ?>> R getReference(Property<?, ?> property, Class<R> referenceType) {
@@ -79,6 +92,20 @@ public class Context {
 		return referenceType.cast(this.keys.get(property));
 	}
 
+	/**
+	 * Returns a new {@link Stream} for all {@link PropertyReference}s stored by this {@link Context}.
+	 *
+	 * @return A new {@link Stream} instance, never null
+	 */
+	public Stream<? extends PropertyReference<?, ?>> stream() {
+		return this.keys.values().stream();
+	}
+
+	/**
+	 * Returns a new {@link Iterator} for all {@link PropertyReference}s stored by this {@link Context}.
+	 *
+	 * @return A new {@link Iterator} instance, never null
+	 */
 	public Iterator<? extends PropertyReference<?, ?>> iterator() {
 		return this.keys.values().iterator();
 	}
@@ -87,10 +114,23 @@ public class Context {
 		return union(Context.of(references));
 	}
 
+	/**
+	 * Returns a new {@link Context} containing the {@link PropertyReference}s of both this {@link Context} as well as
+	 * the ones of the given {@link Context}.
+	 * <p>
+	 * If there is a {@link PropertyReference} in both {@link Context}s for the same {@link Property}, the one of the
+	 * given {@link Context} is taken.
+	 *
+	 * @param other
+	 * 			The other {@link Context} to unionize with; might be null
+	 * @return A new {@link Context} instance, never null
+	 */
 	public Context union(Context other) {
 		Context context = new Context();
 		context.keys.putAll(this.keys);
-		context.keys.putAll(other.keys);
+		if (other != null) {
+			context.keys.putAll(other.keys);
+		}
 		return context;
 	}
 
@@ -121,14 +161,14 @@ public class Context {
 
 	@Override
 	public String toString() {
-		return "DefaultContext [propertyKeys=" + keys + "]";
+		return "Context [propertyKeys=" + keys + "]";
 	}
 
 	/**
-	 * Creates a new {@link Context} using the given indices.
+	 * Creates a new {@link Context} using the given {@link PropertyReference}s.
 	 *
 	 * @param references
-	 *            The {@link PropertyReference}s to create a new context from; might be null or contain null values.
+	 * 			The {@link PropertyReference}s to create a new context from; might be null, empty or contain null values.
 	 * @return A new {@link Context} of the given references, never null
 	 */
 	@SafeVarargs
